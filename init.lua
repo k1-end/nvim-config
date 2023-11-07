1df43b94494319d68e2334cb5343aedeb4a57b99
Keyvan Poursoltani <keyvan.soltan18@gmail.com>
2023-11-07 07:46:09 +0330

    theme change

--- init.lua
+++ init.lua
@@ -150,7 +150,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'catppuccin-latte',
         component_separators = '|',
         section_separators = '',
       },
@@ -160,11 +160,10 @@ require('lazy').setup({
   {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
+    main = "ibl",
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
     opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
     },
   },
 
