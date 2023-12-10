806beb1b3e362624dd0f4dc028e6090419a05cf7
Keyvan Poursoltani <keyvan.soltan18@gmail.com>
2023-12-11 00:14:28 +0330

    set keymap for showing hover info

--- lua/k1/remap.lua
+++ lua/k1/remap.lua
@@ -1,3 +1,8 @@
 vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<C-l>', ':noh<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
+    end,
+  })
