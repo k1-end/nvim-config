6e04b3b74df4370e7afa35db5c2156e4c31202cc
Keyvan Poursoltani <keyvan.soltan18@gmail.com>
2023-12-11 21:35:35 +0330

    set tab options

--- lua/k1/settings.lua
+++ lua/k1/settings.lua
@@ -1,5 +1,15 @@
-vim.cmd("set relativenumber")
-vim.cmd("set nu")
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4 -- tab size
+vim.opt.softtabstop = 4 -- how many tabs to insert when pressing tab
+vim.opt.shiftwidth = 4 -- auto-indentation 
+vim.opt.expandtab = true -- replace tabs with spaces
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
 vim.cmd("set foldmethod=indent")
 vim.cmd("colorscheme tokyonight")
 vim.cmd("autocmd BufRead * normal 2zr")
